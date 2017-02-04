aa947e98fe173b1155d5631ec0dcba120af7d83b
Jesse Novotny <jessenovotny@gmail.com>
2017-02-03 17:28:42 -0700

    fixed typo in mailer

--- app/mailers/lead_magnet_mailer.rb
+++ app/mailers/lead_magnet_mailer.rb
@@ -2,6 +2,6 @@ class LeadMagnetMailer < ApplicationMailer
   def send_playbook(subscriber)
     @subscriber = subscriber
     attachments["budgeting-playbook.pdf"] = File.read('app/assets/documents/budgeting-playbook.pdf')
-    mail(to: subscriber.email, bcc: 'jesse@realbudgeting.com' subject: "Real Budgeting Playbook")
+    mail(to: subscriber.email, bcc: 'jesse@realbudgeting.com', subject: "Real Budgeting Playbook")
   end
 end
